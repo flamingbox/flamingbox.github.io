30af194d94fb6a9dc6443a239ba4f760b07bd038
flamingpaperbox <564307760@qq.com>
2021-07-24 21:52:14 +0800

    blog

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -15,3 +15,24 @@ btn.onclick = function() {
 // document.querySelector('html').onclick = function() {
 //     alert('点你大爷，不点会死啊');
 // }
+
+let myButton = document.getElementById('study');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = '你牛逼大发了，' + myName;
+    }
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = '你牛逼大发了,' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+ }
