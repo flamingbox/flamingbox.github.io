028bdfbef181ae5d95664de786f45ea52246afba
flamingpaperbox <564307760@qq.com>
2021-07-26 22:30:30 +0800

    blog

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,14 @@
-var btn=document.getElementById("btn");
-var pic=document.getElementById("tp");
-var num=1;
-btn.onclick = function() {
-    if(num===1) {
-      pic.src="img/firefox2.png";
-      num=2;
-    } 
+var btn = document.getElementById("btn");
+var pic = document.getElementById("tp");
+var num = 1;
+btn.onclick = function () {
+    if (num === 1) {
+        pic.src = "img/firefox2.png";
+        num = 2;
+    }
     else {
-      pic.src="img/1.png";
-      num=1;
+        pic.src = "img/1.png";
+        num = 1;
     }
 }
 
@@ -20,25 +20,28 @@ let myButton = document.getElementById('study');
 let myHeading = document.querySelector('h1');
 function setUserName() {
     let myName = prompt('请输入你的名字。');
-    if(!myName || myName === null) {
-      setUserName();
+    if (!myName || myName === null) {
+        setUserName();
     } else {
-      localStorage.setItem('name', myName);
-      myHeading.innerHTML = '你牛逼大发了，' + myName;
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = '你牛逼大发了，' + myName;
     }
-  }
-  if(!localStorage.getItem('name')) {
+}
+if (!localStorage.getItem('name')) {
     setUserName();
-  } else {
+} else {
     let storedName = localStorage.getItem('name');
     myHeading.textContent = '你牛逼大发了,' + storedName;
-  }
-  myButton.onclick = function() {
+}
+myButton.onclick = function () {
     setUserName();
- }
- window.onresize = function(){
-    console.log('可视宽'+ document.documentElement.clientWidth);
-    
+}
+window.onresize = function () {
+    console.log('可视宽' + document.documentElement.clientWidth);
+
     console.log('可视高' + document.documentElement.clientHeight);
-    
-    }
+
+    var msg = document.getElementById("message");
+    msg.style.height=document.documentElement.clientHeight-285+"px";
+    console.log('字框高' + msg.style.height);
+}
