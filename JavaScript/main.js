f569378f4fea7c757cd639dd4eb32557d265606f
flamingpaperbox <564307760@qq.com>
2021-07-29 08:07:42 +0800

    blog

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,41 +1,4 @@
-var btn = document.getElementById("btn");
-var pic = document.getElementById("tp");
-var num = 1;
-btn.onclick = function () {
-    if (num === 1) {
-        pic.src = "img/firefox2.png";
-        num = 2;
-    }
-    else {
-        pic.src = "img/1.png";
-        num = 1;
-    }
-}
 
-// document.querySelector('html').onclick = function() {
-//     alert('点你大爷，不点会死啊');
-// }
-
-let myButton = document.getElementById('study');
-let myHeading = document.querySelector('h1');
-function setUserName() {
-    let myName = prompt('请输入你的名字。');
-    if (!myName || myName === null) {
-        setUserName();
-    } else {
-        localStorage.setItem('name', myName);
-        myHeading.innerHTML = '你牛逼大发了，' + myName;
-    }
-}
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = '你牛逼大发了,' + storedName;
-}
-myButton.onclick = function () {
-    setUserName();
-}
 window.onresize = function () {
     console.log('可视宽' + document.documentElement.clientWidth);
 
@@ -50,4 +13,7 @@ window.onresize = function () {
     var mainblock = document.getElementById("mainblock");
     mainblock.style.width=document.documentElement.clientWidth-548+"px";
     console.log('主体宽' + mainblock.style.width);
+    var fox = document.getElementById("fox");
+    fox.style.width=mainblock.style.width;
+    console.log('照片宽' + fox.style.width);
 }
