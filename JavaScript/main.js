47473a269edc1b7fe216f37948791f4175e74e7c
flamingpaperbox <564307760@qq.com>
2021-08-03 18:26:03 +0800

    blog

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -4,16 +4,15 @@ window.onresize = function () {
 
     console.log('可视高' + document.documentElement.clientHeight);
 
-    var msg = document.getElementById("message");
-    msg.style.height=document.documentElement.clientHeight-285+"px";
-    console.log('字框高' + msg.style.height);
     var menu = document.getElementById("menu");
     menu.style.height=document.documentElement.clientHeight-270+"px";
     console.log('菜单高' + menu.style.height);
     var mainblock = document.getElementById("mainblock");
     mainblock.style.width=document.documentElement.clientWidth-548+"px";
     console.log('主体宽' + mainblock.style.width);
-    var fox = document.getElementById("fox");
-    fox.style.width=mainblock.style.width;
-    console.log('照片宽' + fox.style.width);
+    var mainpic = document.getElementsByClassName("mainpic");
+    for (var i = 0; i < mainpic.length; i++) {
+        mainpic[i].style.width=mainblock.style.width;
+    }
+    console.log('照片宽' + mainpic[0,1].style.width);
 }
