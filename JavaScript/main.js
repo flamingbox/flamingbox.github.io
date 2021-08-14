d6215f05bf9c54576e2eaa8d3e2a6fa0c1f23f6e
flamingpaperbox <564307760@qq.com>
2021-08-14 10:26:19 +0800

    blog

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,34 +1,46 @@
 var nta = document.getElementById("NightAnimation");
-   nta.style.width=document.documentElement.clientWidth+"px";
-    console.log('遮罩宽' + nta.style.width);
-    nta.style.height=document.documentElement.clientHeight+"px";
-    console.log('遮罩高' + nta.style.height);
+var suna = document.getElementById("SunAnimation");
+nta.style.width = document.documentElement.clientWidth + "px";
+nta.style.height = document.documentElement.clientHeight + "px";
+console.log('遮罩宽' + nta.style.width);
+console.log('遮罩高' + nta.style.height);
+
 window.onresize = function () {
     console.log('可视宽' + document.documentElement.clientWidth);
-
     console.log('可视高' + document.documentElement.clientHeight);
-
     var menu = document.getElementById("menu");
-    menu.style.height=document.documentElement.clientHeight-270+"px";
+    menu.style.height = document.documentElement.clientHeight - 270 + "px";
     console.log('菜单高' + menu.style.height);
     var mainblock = document.getElementById("mainblock");
-    mainblock.style.width=document.documentElement.clientWidth-548+"px";
+    mainblock.style.width = document.documentElement.clientWidth - 548 + "px";
     console.log('主体宽' + mainblock.style.width);
     var mainpic = document.getElementsByClassName("mainpic");
     for (var i = 0; i < mainpic.length; i++) {
-        mainpic[i].style.width=mainblock.style.width;
+        mainpic[i].style.width = mainblock.style.width;
     }
-    console.log('照片宽' + mainpic[0,1].style.width);
-    var nta = document.getElementById("NightAnimation");
-   nta.style.width=document.documentElement.clientWidth+"px";
-    console.log('遮罩宽' + nta.style.width);
-    nta.style.height=document.documentElement.clientHeight+"px";
-    console.log('遮罩高' + nta.style.height);
+
+
+    nta.style.width = document.documentElement.clientWidth + "px";
+    nta.style.height = document.documentElement.clientHeight + "px";
 }
-nightbuttom.onclick=function(){
-    if(){
-nta.style.display="flex"
-setTimeout(function(){nta.style.display="none"},2000)
+
+var nightbuttom = document.getElementById("nightbuttom");
+nightbuttom.onclick = function () {
+    if (document.getElementById("nightbuttomtext").innerText === "夜间模式") {
+        nta.style.display = "flex"
+        setTimeout(function () { nta.style.display = "none" }, 2000)
+        document.getElementById("nightbuttomtext").innerText = "白昼模式"
+        document.getElementById("nightbuttomicon").style.display="none"
+        document.getElementById("sunbuttomicon").style.display="inline-block"
+        
+    }
+    else {
+        suna.style.display = "flex"
+        setTimeout(function () { suna.style.display = "none" }, 2000)
+        document.getElementById("nightbuttomtext").innerText = "夜间模式"
+        document.getElementById("sunbuttomicon").style.display="none"
+        document.getElementById("nightbuttomicon").style.display="inline-block"
+        
     }
 }
 
