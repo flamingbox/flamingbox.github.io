0e83890eeb8f689d1c3d89f6f63b5cef40d00566
flamingpaperbox <564307760@qq.com>
2021-11-02 13:56:41 +0800

    11.1

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,9 @@
 var nta = document.getElementById("NightAnimation");
 var suna = document.getElementById("SunAnimation");
+var menu = document.getElementById("menu");
+var mainpic = document.getElementsByClassName("mainpic");
+var mainblock = document.getElementById("mainblock");
+var demo = document.getElementById("demo");
 nta.style.width = document.documentElement.clientWidth + "px";
 nta.style.height = document.documentElement.clientHeight + "px";
 console.log('遮罩宽' + nta.style.width);
@@ -8,15 +12,13 @@ console.log('遮罩高' + nta.style.height);
 window.onresize = function () {
     console.log('可视宽' + document.documentElement.clientWidth);
     console.log('可视高' + document.documentElement.clientHeight);
-    var menu = document.getElementById("menu");
     menu.style.height = document.documentElement.clientHeight - 270 + "px";
     console.log('菜单高' + menu.style.height);
-    var mainblock = document.getElementById("mainblock");
-    mainblock.style.width = document.documentElement.clientWidth - 548 + "px";
-    console.log('主体宽' + mainblock.style.width);
-    var mainpic = document.getElementsByClassName("mainpic");
+    demo.style.width = (document.documentElement.clientWidth - 548)/2 + "px";
+    console.log('主体宽' + demo.style.width);
+
     for (var i = 0; i < mainpic.length; i++) {
-        mainpic[i].style.width = mainblock.style.width;
+        mainpic.style.motyle= demo.style.width;
     }
 
 
@@ -25,10 +27,11 @@ window.onresize = function () {
 }
 
 var nightbuttom = document.getElementById("nightbuttom");
+
 nightbuttom.onclick = function () {
     if (document.getElementById("nightbuttomtext").innerText === "夜间模式") {
         nta.style.display = "flex"
-        setTimeout(function () { nta.style.display = "none" }, 2000)
+        setTimeout(function () { nta.style.display = "none" }, 1500)
         document.getElementById("nightbuttomtext").innerText = "白昼模式"
         document.getElementById("nightbuttomicon").style.display="none"
         document.getElementById("sunbuttomicon").style.display="inline-block"
@@ -36,7 +39,7 @@ nightbuttom.onclick = function () {
     }
     else {
         suna.style.display = "flex"
-        setTimeout(function () { suna.style.display = "none" }, 2000)
+        setTimeout(function () { suna.style.display = "none" }, 1500)
         document.getElementById("nightbuttomtext").innerText = "夜间模式"
         document.getElementById("sunbuttomicon").style.display="none"
         document.getElementById("nightbuttomicon").style.display="inline-block"
